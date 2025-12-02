9514db5a93836ba8b9df14f618fd8af6501ba4ab
Roger Chen <rogermchen.ubc@gmail.com>
2025-12-02 13:32:57 -0700

    update

---
+++ lib/data/services/db.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DB {
+  static final DB instance = DB._internal();
+
+  static Database? _db;
+
+  DB._internal();
+
+  Future<Database> get database async {
+    if (_db != null) return _db!;
+
+    _db = await _initDB();
+    return _db!;
+  }
+
+  Future<Database> _initDB() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, 'life.db');
+
+    final exists = await databaseExists(path);
+    if (!exists) {
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+      } catch (_) {}
+
+      ByteData data = await rootBundle.load(join('assets', 'db', 'life.db'));
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      await File(path).writeAsBytes(bytes, flush: true);
+    }
+    return await openDatabase(path);
+  }
+}
