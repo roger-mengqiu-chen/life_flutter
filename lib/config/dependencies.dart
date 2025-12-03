76addd8728f921985edd2079ac3a82af5437c5a4
Roger Chen <rogermchen.ubc@gmail.com>
2025-12-03 10:26:50 -0700

    use dependency injection

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -1,10 +1,12 @@
 import 'package:life_flutter/data/repositories/category_repository.dart';
+import 'package:life_flutter/data/services/db.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 List<SingleChildWidget> get providers {
   return [
-    Provider(create: (context) => CategoryRepository()),
+    Provider(create: (context) => DB.instance),
+    Provider(create: (context) => CategoryRepository(db: context.read<DB>())),
   ];
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:life_flutter/data/services/db.dart';
 import 'package:life_flutter/ui/home/widgets/home_screen.dart';
+import 'package:life_flutter/config/dependencies.dart';
+import 'package:provider/provider.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DB.instance.database;
-  runApp(const MainApp());
+  runApp(MultiProvider(providers: providers, child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
