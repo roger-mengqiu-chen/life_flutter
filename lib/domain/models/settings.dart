ad7aedac18e1db5dae086c0e99bdc39862f7f32a
Roger Chen <rogermchen.ubc@gmail.com>
2025-12-03 12:18:50 -0700

    updated settings class

--- lib/domain/models/settings.dart
+++ lib/domain/models/settings.dart
@@ -2,6 +2,17 @@ class Settings {
   late String name;
   late String value;
 
+  Map<String, dynamic> get entry {
+    dynamic result;
+    if(value == '1' || value == '0') {
+      result = value == '1';
+    }
+    else {
+      result = value;
+    }
+    return {name: result};
+  }
+
   @override
   String toString() {
     return '$name - $value';
