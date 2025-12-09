27d1b6bdc45823bef4b157f5a06ea096c479beaf
Roger Chen <rogermchen.ubc@gmail.com>
2025-12-09 09:07:40 -0700

    created transaction viewmodel

--- lib/ui/transactions/viewmodels/transaction_viewmodel.dart
+++ lib/ui/transactions/viewmodels/transaction_viewmodel.dart
@@ -1,3 +1,14 @@
-class TransactionViewmodel {
+import 'package:life_flutter/data/repositories/transaction_repository.dart';
+import 'package:life_flutter/domain/models/transaction.dart';
 
+class TransactionViewmodel {
+  TransactionViewmodel({
+    required TransactionRepository transactionRepository
+  }) : _transactionRepository = transactionRepository;
+
+  final TransactionRepository _transactionRepository;
+
+  Future<List<Transaction>> get transactions async {
+    return await _transactionRepository.transactions;
+  }
 }
