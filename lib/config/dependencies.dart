7882e5497ecc22127aa797ee66239f505bd0d50a
Roger Chen <rogermchen.ubc@gmail.com>
2025-12-09 10:20:12 -0700

    inject TransactionRepository

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -1,6 +1,7 @@
 import 'package:life_flutter/data/repositories/auth_repository.dart';
 import 'package:life_flutter/data/repositories/category_repository.dart';
 import 'package:life_flutter/data/repositories/setting_repository.dart';
+import 'package:life_flutter/data/repositories/transaction_repository.dart';
 import 'package:life_flutter/data/services/auth.dart';
 import 'package:life_flutter/data/services/db.dart';
 import 'package:provider/provider.dart';
@@ -12,6 +13,7 @@ List<SingleChildWidget> get providers {
     Provider(create: (context) => BiometricService()),
     Provider(create: (context) => CategoryRepository(db: context.read())),
     Provider(create: (context) => SettingRepository(db: context.read())),
+    Provider(create: (context) => TransactionRepository(db: context.read())),
     ChangeNotifierProvider(
       create: (context) => AuthRepository(
         db: context.read(),
